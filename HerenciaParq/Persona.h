243708e013381bc5179a8343a9bfae02952dfce4
jmlopezriani <jmlopezriani@javerianacali.edu.co>
2021-09-15 16:02:41 -0500

    creacion clases

---
+++ HerenciaParq/Persona.h
@@ -0,0 +1,19 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+class Persona
+{
+private:
+    /* data */
+public:
+    Persona(/* args */);
+    ~Persona();
+};
+
+Persona::Persona(/* args */)
+{
+}
+
+Persona::~Persona()
+{
+}
+#endif
